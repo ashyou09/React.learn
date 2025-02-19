7a477e43203b55e578fbe29438ef9987d0a557a6
Ashutosh Singh <ash.2024@nst.rishihood.edu.in>
2025-02-19 19:37:09 +0530

    done learning render list

--- render-list-2/src/App.jsx
+++ render-list-2/src/App.jsx
@@ -1,8 +1,27 @@
 import List from './list.jsx';
 
-function App(){
-  return(
-    <List/>
-  )
+function App(props){
+
+  const Fruits = [
+    {id:1 , name :"apple" , calories : 90},
+     { id : 2,  name : "mango", calories : 45} ,
+      {id : 3,name : "banana", calories : 100} ,
+       { id :4 , name : "pineapple", calories : 50}];
+
+  const Vegetable = [
+    {id:6 , name:'carrot' , calories: 30},
+    { id :7,  name : 'broccoli', calories : 20} ,
+    {id :8,name : 'potato', calories : 40} ,
+    { id :9, name : 'cucumber', calories : 10}];
+
+  // return(<>
+  // {Fruits.length > 0 ?<List items={Fruits} category="Fruits" /> : null}
+  // {Vegetable.length > 0 ? <List items={Vegetable} category="Vegetable" /> : null}
+  //   </> );
+    // Or you can use both be true
+    return(<>
+      {Fruits.length > 0 && <List items={Fruits} category="Fruits" />}
+      {Vegetable.length > 0 && <List items={Vegetable} category="Vegetables" />}
+        </> );
 }
 export default App;
--- render-list-2/src/list.jsx
+++ render-list-2/src/list.jsx
@@ -1,30 +1,32 @@
+import PropTypes from 'prop-types' ;
 
+function List(props){  
+    const category = props.category ; 
+    
+    const itemList = props.items ;
 
-function List(){
-    // const fruits = ["apple" , "mango" , "banana" , "pineapple"] ; // list now goint to convert it in to object
-    const fruits = [
-        {id:1 , name :"apple" , calories : 90},
-         { id : 2,  name : "mango", calories : 45} ,
-          {id : 3,name : "banana", calories : 100} ,
-           { id :4 , name : "pineapple", calories : 50}];
-
-    // fruits.sort((a,b)=> a.name.localeCompare(b.name)); // this can sort the list in alphabetical order to there name .
-    // fruits.sort((b,a)=> a.name.localeCompare(b.name)); // this can sort the list in reverse alphabetical order to there name .
-    // fruits.sort((a,b)=> a.calories - b.calories); // this can sort the list in calories in numerical order Aescending
-
-
-    // const highcalories = fruits.filter(fruit => fruit.calories > 50) ;
-    // const fruitsList = highcalories.map(highcalories => <li key = {highcalories.id}>
-    //     {highcalories.name}:&nbsp;
-    //     <b>{highcalories.calories}</b>
-    //     </li>);    
-
-    const fruitsList = fruits.map(fruit => <li key = {fruit.id}>
-                                                {fruit.name}:&nbsp;
-                                                <b>{fruit.calories}</b>
+    const listitem = itemList.map(itemList => <li key = {itemList.id}>
+                                                {itemList.name}:&nbsp;
+                                                <b>{itemList.calories}</b>
                                                 </li>);// nbsp :- none breking space 
 
-    return(<ol>{fruitsList}</ol>);
+    return(
+    <>
+    <h3 className = "list-category">{category}</h3>
+    <ol className = "list-item">{listitem}</ol>
+    </>
+    );
+}
+List.proptypes = { 
+    category : PropTypes.string,
+    items : PropTypes.arrayOf(PropTypes.shape({id : PropTypes.number,
+                                             name : PropTypes.string,
+                                            calories : PropTypes.number}))
+    }
+
+List.DefualtProps = {
+    category : "Category",
+    items : []
 }
 
 export default List;
